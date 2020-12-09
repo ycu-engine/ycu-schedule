9e29a758ffcb68add99db43c06fc5ae9f58f104b
YutaUra <yuuta3594@outlook.jp>
2020-12-10 05:02:21 +0900

    initial

---
+++ web/pages/about.tsx
@@ -0,0 +1,16 @@
+import Layout from '@/components/Layout'
+import Link from 'next/link'
+
+const AboutPage = () => (
+  <Layout title='About | Next.js + TypeScript Example'>
+    <h1>About</h1>
+    <p>This is the about page</p>
+    <p>
+      <Link href='/'>
+        <a>Go home</a>
+      </Link>
+    </p>
+  </Layout>
+)
+
+export default AboutPage
