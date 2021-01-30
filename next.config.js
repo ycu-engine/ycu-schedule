0defc5e210c29fb48980b2c571fc19708eb2b1e2
YutaUra <yuuta3594@outlook.jp>
2021-01-31 00:10:30 +0900

    fix build env variables

--- next.config.js
+++ next.config.js
@@ -4,5 +4,6 @@ module.exports = {
     AZURE_CLIENT_ID: process.env.AZURE_CLIENT_ID,
     AZURE_CLIENT_SECRET: process.env.AZURE_CLIENT_SECRET,
     AZURE_TENANT_ID: process.env.AZURE_TENANT_ID,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
   },
 }
