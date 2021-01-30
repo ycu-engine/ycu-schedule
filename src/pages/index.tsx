520786a99f746acc1832053a901f5095d1c2a12e
YutaUra <yuuta3594@outlook.jp>
2021-01-30 23:54:54 +0900

    fix serverless yml

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { signIn, signOut, useSession } from 'next-auth/client'
 import { useEffect } from 'react'
 
 const Home = (): JSX.Element => {
-  const [session, loading] = useSession()
+  const [session] = useSession()
 
   useEffect(() => {
     console.log(session?.user)
