96fc673cf6cc5260060bf99fe89614239e70b644
YutaUra <yuuta3594@outlook.jp>
2021-03-11 07:06:30 +0900

    origin request policy

--- resources/cloudfront.ts
+++ resources/cloudfront.ts
@@ -96,10 +96,10 @@ const cloudfront: Serverless = {
           HeadersConfig: {
             HeaderBehavior: "whitelist",
             Headers: [
-              "accept-encoding",
-              "origin",
-              "access-control-request-headers",
-              "access-control-request-method",
+              "Accept-Encoding",
+              // "origin",
+              // "access-control-request-headers",
+              // "access-control-request-method",
             ],
           },
           Name: kebabCase2TitleCase(`${service}-${stage}`),
