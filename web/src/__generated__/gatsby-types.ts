3c76236f29aaea3d71025c932937da920600188d
YutaUra <yuuta3594@outlook.jp>
2021-03-11 17:48:14 +0900

    redirect www if prod

--- web/src/__generated__/gatsby-types.ts
+++ web/src/__generated__/gatsby-types.ts
@@ -2868,34 +2868,6 @@ declare namespace GatsbyTypes {
     }
   }
 
-  type ReadmeQueryVariables = Exact<{ [key: string]: never }>
-
-  type ReadmeQuery = {
-    readonly file: Maybe<{
-      readonly childMarkdownRemark: Maybe<
-        Pick<MarkdownRemark, "excerpt" | "htmlAst"> & {
-          readonly frontmatter: Maybe<
-            Pick<MarkdownRemarkFrontmatter, "title" | "updatedAt">
-          >
-        }
-      >
-    }>
-  }
-
-  type NewsTemplateQueryVariables = Exact<{
-    id: Scalars["String"]
-  }>
-
-  type NewsTemplateQuery = {
-    readonly markdownRemark: Maybe<
-      Pick<MarkdownRemark, "excerpt" | "htmlAst"> & {
-        readonly frontmatter: Maybe<
-          Pick<MarkdownRemarkFrontmatter, "date" | "title">
-        >
-      }
-    >
-  }
-
   type GatsbyImageSharpFixedFragment = Pick<
     ImageSharpFixed,
     "base64" | "width" | "height" | "src" | "srcSet"
@@ -2978,4 +2950,32 @@ declare namespace GatsbyTypes {
     ImageSharpFluid,
     "aspectRatio" | "src" | "srcSet" | "srcWebp" | "srcSetWebp" | "sizes"
   >
+
+  type NewsTemplateQueryVariables = Exact<{
+    id: Scalars["String"]
+  }>
+
+  type NewsTemplateQuery = {
+    readonly markdownRemark: Maybe<
+      Pick<MarkdownRemark, "excerpt" | "htmlAst"> & {
+        readonly frontmatter: Maybe<
+          Pick<MarkdownRemarkFrontmatter, "date" | "title">
+        >
+      }
+    >
+  }
+
+  type ReadmeQueryVariables = Exact<{ [key: string]: never }>
+
+  type ReadmeQuery = {
+    readonly file: Maybe<{
+      readonly childMarkdownRemark: Maybe<
+        Pick<MarkdownRemark, "excerpt" | "htmlAst"> & {
+          readonly frontmatter: Maybe<
+            Pick<MarkdownRemarkFrontmatter, "title" | "updatedAt">
+          >
+        }
+      >
+    }>
+  }
 }
--- web/src/lib/msal.ts
+++ web/src/lib/msal.ts
@@ -1,13 +1,19 @@
 import { PopupRequest, PublicClientApplication } from "@azure/msal-browser"
 import { useMsal } from "@azure/msal-react"
 import { useCallback } from "react"
+import { CloudFrontDistributionDomainName } from "~/info"
 
 export const msalClientApp = new PublicClientApplication({
   auth: {
     clientId: "eb7cd567-1d8f-4ca9-a0ba-31919aa6c647",
     authority:
       "https://login.microsoftonline.com/be636d66-cbfe-41b8-895c-ee5cbd8bc755/",
-    redirectUri: "http://localhost:8000/login",
+    redirectUri:
+      process.env.DEPLOY_STAGE === "prod"
+        ? "https://www.ycu-schedule.com"
+        : process.env.NODE_ENV === "production"
+        ? `https://${CloudFrontDistributionDomainName}/login`
+        : "http://localhost:8000/login",
   },
   cache: {
     cacheLocation: "sessionStorage",
